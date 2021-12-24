d0c1708a681994c55000d95a8fcec682f7fe3586
EmersonAraujonb <95513715+EmersonAraujonb@users.noreply.github.com>
2021-12-24 15:25:36 -0300

    Add files via upload
    Calculadora simples

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+const calculadora = {
+    somar : function (a,b) {
+        return a + b;
+    },
+    subtrair : function (a,b) {
+        return a - b;
+    },
+    dividir : function (a,b) {
+        return a / b;
+    },
+    multiplicar : function (a,b) {
+        return a * b;
+    }
+}
+console.log(calculadora.somar(2,4));
+
+console.log(calculadora.subtrair(10,5));
+
+console.log(calculadora.dividir(50,5));
+
+console.log(calculadora.multiplicar(2,4));
